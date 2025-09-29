3bca295e2c47027a5b788d22dd7f1e5a36039538
vanille09 <dmrojasq@gmail.com>
2025-09-29 14:51:09 -0500

    Collectibles
    Hice el código de los Collectibles y se lo puse a cada flor. Taggee bien los Collectibles.

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public enum CollectibleType { NormalFlower, SpecialFlower }
+    public CollectibleType type; // Tipo de coleccionable
+
+    public int points = 10; // Puntos que se ganan
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            //Se suman los puntos
+            GameManager.instance.AddScore(type, points);
+
+            // Se destruye la flor recolectada
+            Destroy(gameObject);
+        }
     }
 }
