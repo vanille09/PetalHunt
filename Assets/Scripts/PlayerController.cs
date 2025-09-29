19aed5a150e742abb759d0108aa9bd29f4233f90
vanille09 <dmrojasq@gmail.com>
2025-09-29 01:19:01 -0500

    La mariquita ya se mueve
    Avance en el código del playercontroller. La mariquita ya se mueve bien. Ajusté bien los colliders tanto de la mariquita como de las paredes.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using UnityEngine.SceneManagement; // Sirve para reiniciar el juego cuando se muere la mariquita
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 5f; // Velocidad de la mariquita
+    private Rigidbody2D rb;
+    private Vector2 moveInput; 
+    private Vector2 moveVelocity;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        // Para leer WASD o flechas
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical"); 
+
+
         
+        moveInput = new Vector2(moveX, moveY).normalized;
+        moveVelocity = moveInput * speed;   
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+
+        Debug.Log("¡Has muerto!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Para reiniciar el nivel
+        
+        }
+
+
     }
 }
