5e42b8d81cf6406ca6ce11421f7d4592317424ee
vanille09 <dmrojasq@gmail.com>
2025-09-30 17:36:06 -0500

    Juego funcional
    Ya colecciona, el score suma, categoricé las flores y dependiendo, gana o suma puntos. Prácticamente, juego terminado.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,73 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class GameManger : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static GameManager instance; // Para acceder desde otros scripts
+
+    public Text normalFlowerText;
+    public Text specialFlowerText;
+    public Text scoreText;
+    public Text messageText;
+
+    private int normalFlowerCount = 0;
+    private int specialFlowerCount = 0;
+    private int score = 0;
+
+    private bool levelCompleted = false;
+
+    void Awake()
     {
-        
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateUI()
     {
-        
+        normalFlowerText.text = "Normal Flowers: " + normalFlowerCount;
+        specialFlowerText.text = "Special Flowers: " + specialFlowerCount;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void AddScore(Collectible.CollectibleType type, int points)
+    {
+        switch (type)
+        {
+            case Collectible.CollectibleType.NormalFlower:
+                normalFlowerCount++;
+                break;
+
+            case Collectible.CollectibleType.SpecialFlower:
+                specialFlowerCount++;
+                Victory();
+                break;
+        }
+
+        score += points;
+        UpdateUI();
+    }
+
+    public void Victory()
+    {
+        if (levelCompleted) return;
+        levelCompleted = true;
+
+        if (messageText != null) messageText.text = "¡Ganaste! 🎉";
+        Debug.Log("Victory!");
+        Time.timeScale = 0f;
+    }
+
+    public void GameOver()
+    {
+        if (levelCompleted) return;
+        levelCompleted = true;
+
+        if (messageText != null) messageText.text = "Game Over ☠️";
+        Debug.Log("GameOver");
+        Time.timeScale = 0f;
     }
 }
+
+
+    
