1762e53078095f05589f5edafd2fd4769f5354c8
JJuandi14 <jdalferezs@eafit.edu.co>
2025-10-23 15:51:39 -0500

    Botones
    Las 3 versiones de cada botón (normal, highlighted y presionado)

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -1,16 +1,48 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
 
 public class Buttons : MonoBehaviour
 {
+
+    public GameObject CreditsPanel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GoMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void CreditsOn()
+    {
+        CreditsPanel.SetActive(true);
+    }
+
+    public void CreditsOff()
+    {
+        CreditsPanel.SetActive(false);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
